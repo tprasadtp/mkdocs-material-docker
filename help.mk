7d9e580bdbf5b7cfb5f834b7da16a43e05269c24
Prasad Tengse <tprasadtp@users.noreply.github.com>
2020-03-08 18:57:04 +0100

    update missing basefile

--- help.mk
+++ help.mk
@@ -1,18 +1,18 @@
-# Set default goal to help.
-.DEFAULT_GOAL := help
+# BEGIN-HELP-TEMPLATE
+
+# Set default goal to help, if not set already
+.DEFAULT_GOAL ?= help
 
 # Set the shell
 SHELL := /bin/bash
 
-# Because this is our base of operations :P
-WATCHTOWER_BASE := $(strip $(patsubst %/, %, $(dir $(realpath $(firstword $(MAKEFILE_LIST))))))
 # SEMVER_REGEX := ^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$
 
 .PHONY: help
 help: ## This help dialog.
 	@IFS=$$'\n' ; \
-    help_lines=(`fgrep -h "##" $(MAKEFILE_LIST) | fgrep -v fgrep | sed -e 's/\\$$//' | sed -e 's/##/:/'`); \
-	  printf "%-32s %s\n" " Target " "    Help " ; \
+    help_lines=(`fgrep -h "##" $(MAKEFILE_LIST) | fgrep -v fgrep | sed -e 's/\\$$//' | sed -e 's/##/:/' | sort -u`); \
+	printf "%-32s %s\n" " Target " "    Help " ; \
     printf "%-32s %s\n" "--------" "------------" ; \
     for help_line in $${help_lines[@]}; do \
         IFS=$$':' ; \
@@ -24,3 +24,5 @@ help: ## This help dialog.
         printf '\033[0m'; \
         printf "%s\n" $$help_info; \
     done
+
+# BEGIN-HELP-TEMPLATE
